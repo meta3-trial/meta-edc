1be9a1ee57da1af1a493a8a6df7d7127a5e8e294
Erik van Widenfelt <ew2789@gmail.com>
2021-06-17 12:34:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,18 +15,18 @@ with open(os.path.join(os.path.dirname(__file__), "VERSION")) as f:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="meta-edc",
+    name="meta3-edc",
     version=VERSION,
     author=u"Erik van Widenfelt",
     author_email="ew2789@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    url="https://github.com/meta-trial/meta-edc",
+    url="https://github.com/meta3-trial/meta-edc",
     license="GPL license, see LICENSE",
     description="META Trial EDC (http://www.isrctn.com/ISRCTN76157257)",
     long_description=README,
     zip_safe=False,
-    keywords="django meta EDC",
+    keywords="django meta3 EDC",
     install_requires=[],
     classifiers=[
         "Environment :: Web Environment",
@@ -35,10 +35,11 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     test_suite="runtests.main",
 )
